b5441963e1ffbe328dec7efd6ed71874d8da0727
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-01-20 22:26:06 -0600

    Divisible Sum Pairs January 20, 2022

---
+++ DivisibleSumPairs/DivisibleSumPairs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleSumPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int n: length of the array
+            // int ar[n]: an array of integers
+            // int k: the integer divisor
+
+            Console.WriteLine("Type the length of the array");
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine("Type the numbers into the array separated by a blank-space");
+            List<int> arN = Console.ReadLine().TrimEnd()
+                            .Split(' ').ToList()
+                            .Select(stemp => Convert.ToInt32(stemp))
+                            .ToList();
+
+            Console.WriteLine("Type integer divisor");
+            int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.divisibleSumPairs(n, k, arN);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DivisibleSumPairs/DivisibleSumPairs/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleSumPairs
+{
+    internal class Result
+    {
+        public static int divisibleSumPairs(int n, int k, List<int> list)
+        {
+            int totalPairs = 0;
+            int[] arr = list.ToArray();
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    if((arr[i] + arr[j]) % k == 0)
+                        totalPairs++;
+                }
+            }
+
+            return totalPairs;
+        }
+        
+    }
+}
