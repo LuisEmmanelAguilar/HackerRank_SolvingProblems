e30383180873ccfcd1d40611db39130a81f79ac3
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-01-22 13:57:51 -0600

    DivisionBill

---
+++ BillDivision/BillDivision/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please inter the cost of each item separated by a space blank.");
+            List<int> listItems = Console.ReadLine().TrimEnd()
+                                  .Split(' ').ToList()
+                                  .Select(itemTemp => Convert.ToInt32(itemTemp))
+                                  .ToList();
+
+            Console.WriteLine("Please enter the number of the item of the bill, that Anna didn't shared with Brian");
+            int itemNotShared = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter amount that Brian charged to Anna");
+            int chargedAnna = Convert.ToInt32(Console.ReadLine());
+
+            Result.bonAppetit(listItems, chargedAnna, itemNotShared);
+
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ BillDivision/BillDivision/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillDivision
+{
+    internal class Result
+    {
+        public static void bonAppetit(List<int> billItems, int chargedAnna, int itemNotShared)
+        {
+            int[] arrItems = billItems.ToArray();
+            int amountBill = 0;
+
+            for(int i = 0; i < arrItems.Length; i++)
+                if (arrItems[i] != arrItems[itemNotShared])
+                    amountBill+=arrItems[i];
+
+            if(amountBill / 2 == chargedAnna)
+                Console.WriteLine("Bon Appetit");
+            else
+                Console.WriteLine(Convert.ToString(chargedAnna - (amountBill / 2)));
+
+
+        }
+    }
+}
