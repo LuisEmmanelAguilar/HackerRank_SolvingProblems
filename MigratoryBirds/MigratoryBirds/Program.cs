650974a710d033324b44b5b13676957c1e85ea98
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-01-21 18:22:15 -0600

    Migraroty Birds done!

---
+++ MigratoryBirds/MigratoryBirds/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigratoryBirds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type the length of the array");
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Console.WriteLine("Type the numbers into the array separated by a blank-space");
+            List<int> listBirds = Console.ReadLine().TrimEnd()
+                                .Split(' ').ToList()
+                                .Select(stemp => Convert.ToInt32(stemp))
+                                .ToList();
+
+            int total = ResultMigratoryBirds.maxSightedBird(listBirds);
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MigratoryBirds/MigratoryBirds/ResultMigratoryBirds.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigratoryBirds
+{
+    internal class ResultMigratoryBirds
+    {
+        public static int maxSightedBird(List<int> arr)
+        {
+            int[] count = new int[arr.Max()];
+            int max = 0, c = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                count[arr[i] - 1]++;
+            }
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (max < count[i]) { max = count[i]; c = i; }
+            }
+
+            return c + 1;
+        }
+    }
+}
