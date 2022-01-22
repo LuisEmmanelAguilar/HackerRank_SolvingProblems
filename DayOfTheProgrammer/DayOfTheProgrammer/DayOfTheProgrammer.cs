9c59396b717704a73a26b95f63352adaae421407
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-01-21 19:18:09 -0600

    HackerRank

---
+++ DayOfTheProgrammer/DayOfTheProgrammer/DayOfTheProgrammer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfTheProgrammer
+{
+    internal class DayOfTheProgrammer
+    {
+        public static string programmerDay(int year)
+        {
+            if (year < 1918)
+                return year % 4 == 0 ? "12.09." + year : "13.09." + year;
+            if (year == 1918)
+                return "26.09.1918";
+            else 
+                return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? "12.09." + year : "13.09." + year;  
+
+        }
+
+    }
+}
---
+++ DayOfTheProgrammer/DayOfTheProgrammer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfTheProgrammer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // leap year is divisible by 4
+            // for example 1984 is divisible by 4
+            Console.WriteLine("Please enter a year to determine wich date is the Day of the Programmer in the chosen year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year.ToString().Length > 4)
+                throw new Exception("The year must be 4 digits");
+
+            string day = DayOfTheProgrammer.programmerDay(year);
+            Console.WriteLine(day);
+            Console.ReadLine();
+
+        }
+    }
+}
