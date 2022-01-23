07fff913b24213fcf0e2b89fc91e6f1b8404e4a6
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-01-22 19:11:22 -0600

    Sales By Match

---
+++ SalesByMatch/SalesByMatch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesByMatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elemts in the list");
+            int numberItems = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type the list of itmes separated by a blank space");
+            List<int> listItems = Console.ReadLine().TrimEnd().Split(' ')
+                                  .Select(x => Convert.ToInt32(x)).ToList();
+
+            int total = 0;
+
+            if (numberItems < listItems.Count)
+                throw new Exception("The are more items than the number required.");
+            else if (numberItems > listItems.Count)
+                throw new Exception("The are less items than the number required.");
+            else
+                total = Result.TotalPairs(listItems);
+            
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SalesByMatch/SalesByMatch/Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesByMatch
+{
+    internal class Result
+    {
+        public static int TotalPairs(List<int> listItems)
+        {
+            int[] arr = listItems.ToArray();
+            int total = 0;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] != 0)
+                {
+                    for (int j = i + 1; j < arr.Length; j++)
+                    {
+                        if (arr[i] == arr[j])
+                        {
+                            total++;
+                            arr[j] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return total;
+
+        }
+    }
+}
