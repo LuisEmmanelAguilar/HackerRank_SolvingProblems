974ec4714e9d3eff46dc151a3aac2087b21d34b0
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-01-22 20:55:37 -0600

    Drawing Book

---
+++ DrawingBook/DrawingBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, give the length of the Book's pages");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What number page would you like to open?");
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            int total = Result.totalTurnPages(n, p);
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DrawingBook/DrawingBook/Result.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingBook
+{
+    internal class Result
+    {
+        public static int totalTurnPages(int total, int target)
+        {
+            var turnPages = 0;
+
+            if(target == 1 || total == target)
+                return turnPages;
+            if (total % 2 != 0 && target == total - 1)
+                return turnPages;
+
+
+            if(total % 2 == 0)
+            {
+                if(target <= total / 2)
+                {
+                    // forward
+                    turnPages = target / 2;
+                }
+                else
+                {
+                    //backwards
+                    double d = ((double)(total - target) / 2);
+                    turnPages = (int)Math.Ceiling(d);
+                }
+            }
+            else
+            {
+                if(target == (total / 2) + 1 && total % 4 == 3)
+                {
+                    turnPages = (total - target) / 2;
+                }
+                else
+                {
+                    if(target <= (total / 2) + 1)
+                    {
+                        // from start
+                        turnPages = target / 2;
+                    }
+                    else
+                    {
+                        // end to start
+                        turnPages = (total - target) / 2;
+                    }
+                }
+            }
+
+
+            return turnPages;
+                
+        }
+
+    }
+}
